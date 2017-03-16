bc1201e0dc56572877c31964e262d0a093a08e1d
Daniel Weiner <daniel.weiner@breaktech.com>
2017-03-16 14:55:20 -0500

    Implement Neo4JStrategy

--- src/Neo4JStrategy.php
+++ src/Neo4JStrategy.php
@@ -1,6 +1,27 @@
 <?php
+use \GraphAware\Neo4j\Client\Client;
 
-class Neo4JStrategy
+class Neo4JStrategy implements Neo4JStrategyInterface
 {
 
+    /**
+     * @var Neo4JStrategyClosure[]
+     */
+    private $closures;
+    /**
+     * @return Neo4JStrategyClosure[]
+     */
+    public function getClosures()
+    {
+        return $this->closures;
+    }
+
+    /**
+     * @param callable $closure
+     * @return Neo4JStrategyInterface
+     */
+    public function add(callable $closure)
+    {
+        $this->closures[] = new Neo4JStrategyClosure($closure);
+    }
 }
