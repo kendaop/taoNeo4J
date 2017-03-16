ff1096abab28d8a897dc2c040d11a49130d27c5b
Daniel Weiner <daniel.weiner@breaktech.com>
2017-03-16 16:25:31 -0500

    Implement some more query building

--- src/Neo4JQueryBuilder.php
+++ src/Neo4JQueryBuilder.php
@@ -1,11 +1,23 @@
 <?php
 class Neo4JQueryBuilder
 {
-    public static function insert($data) {
+    public static function insertNode($data) {
+        $query = "CREATE (item {properties})";
+        $params = ['properties' => $data];
 
+        return [$query, $params];
     }
 
-    public static function query($data) {
+    public static function find($id) {
+        $query = 'MATCH (item) WHERE item.id = $id RETURN item';
+        $params = ['id' => $id];
 
+        return [$query, $params];
+    }
+
+    public static function findRelationship($subject, $object, $relationship) {
+        $query = 'MATCH (subject {subject})-[r:RELATES_TO {relationship}]->(object {object}) RETURN properties(r) ';
+        $params = ['subject' => [$subject], 'object' => [$object], 'relationship' => $relationship];
+        return [$query, $params];
     }
 }
--- src/Neo4JQueryRunner.php
+++ src/Neo4JQueryRunner.php
@@ -1,14 +1,55 @@
 <?php
 use GraphAware\Neo4j\Client\Client;
-
+use \GraphAware\Neo4j\Client\Stack;
+use \GraphAware\Bolt\Result\Result;
 class Neo4JQueryRunner
 {
+    protected function pushStack(Stack $stack, $params) {
+        return call_user_func_array([$stack, 'push'], $params);
+    }
+
     public static function insertRecord(Client $client, $modelId, $subject, $object, $predicate, $language, $author, $epoch) {
-        $strategy = Neo4JStrategyBuilder::create()->add(function(Client $client, $data) {
+        $data = [
+            'modelId' => $modelId,
+            'subject' => $subject,
+            'object' => $object,
+            'predicate' => $predicate,
+            'language' => $language,
+            'author' => $author,
+            'epoch' => $epoch
+        ];
+
+        $strategy = Neo4JStrategyBuilder::create()->add(function(Client $client, $data) use ($subject) {
             $stack = $client->stack();
-            call_user_func_array([$stack, 'push'], Neo4JQueryBuilder::insert($data));
+            // see if the Subject node exists
+            static::pushStack($stack, Neo4JQueryBuilder::find($subject));
             return $stack;
+        })->add(function (Client $client, Result $data) use ($subject, $object) {
+            $stack = $client->stack();
+
+            // if the subject node doesn't exist, insert it
+            if (empty($data->getRecords())) {
+                static::pushStack($stack, Neo4JQueryBuilder::insertNode(['id' => $subject]));
+            }
+
+            // see if the object node exists
+            static::pushStack($stack, Neo4JQueryBuilder::find($object));
+            return $stack;
+        })->add(function (Client $client, Result $data) use ($subject, $object, $predicate) {
+            $stack = $client->stack();
+
+            // if the object node doesn't exist, insert it
+            if (empty($data->getRecords())) {
+                static::pushStack($stack, Neo4JQueryBuilder::insertNode(['id' => $object]));
+            }
+
+            // find the relationship between the subject and the object
+            static::pushStack($stack, Neo4JQueryBuilder::findRelationship($subject, $object, [
+                'id' => $predicate
+            ]));
         });
+
+        return static::run($client, $data, $strategy);
     }
 
     public static function run(Client $client, $data, Neo4JStrategyInterface $strategy) {
