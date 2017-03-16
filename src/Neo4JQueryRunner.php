313430c39a2000c13353071dc34d3a9984d5c1f6
Daniel Weiner <daniel.weiner@breaktech.com>
2017-03-16 14:24:08 -0500

    More basic functionality

--- src/Neo4JQueryRunner.php
+++ src/Neo4JQueryRunner.php
@@ -3,6 +3,10 @@ use GraphAware\Neo4j\Client\Client;
 
 class Neo4JQueryRunner
 {
+    public static function insertRecord($modelId, $subject, $object, $predicate, $language, $author, $epoch) {
+
+    }
+
     public static function run(Client $client, $data, Neo4JStrategyInterface $strategy) {
         foreach ($strategy->getClosures() as $closure) {
             $data = $closure($data, $client);
